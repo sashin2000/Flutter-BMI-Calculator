952d85a2d717aad76b5345d75270fce2bff39e94
sashin2000 <sashinsirichandra@gmail.com>
2021-02-19 23:17:23 +0530

    complete first screen of the bmi calculator

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kBottomContainerColor = Color(0xFFEB1555);
+const kActiveCardColor = Color(0xFF111928);
+const kInactiveCardColor = Color(0xFF141A3C);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
